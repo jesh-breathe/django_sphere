17d16124ca2faaa8415bb86680e86d7fbf6a8f8a
jeshurun-com <jeshurunasefa@gmail.com>
2024-09-08 13:33:03 +0300

    three of them

---
+++ frontend/src/components/Vision.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Vision() {
+  return (
+    <section className='flex flex-col justify-center items-center py-8 px-4'>
+        <h2 className='text-4xl font-semibold mb-4 text-center font-trebuchetMs text-gray-800'>Our Vision</h2>
+        <p className='border-2 p-2 rounded-lg border-gray-300 max-w-3xl font-trebuchetMs text-lg text-gray-700 text-center leading-relaxed'>LCAACST is for not-profit; non-governmental charity organization locally
+            registered on August 20, 2019 and accorded legal personality with registration
+            number 4372. LCAACST is emulated from the global successes stories of Australia
+            and Germany land cares’ profound achievement that has been adopted
+            worldwide by many countries and land managers for their fruitful collective
+            action. LCAACST is thus committed to make our city Addis Ababa and
+            surrounding towns’ neat, green, climate resilient and priority choice to live among
+            cities of the world through “collective action”. 
+        </p>
+    </section>
+  )
+}
+
+export default Vision
