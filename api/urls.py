de305403443930c244c8686802bd9f448e5470a8
jeshurun-com <jeshurunasefa@gmail.com>
2024-09-26 23:25:24 +0300

    1/4

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from api.views import kind_detail, kind_list, create_kind
+from api.views import kind_list, create_kind, kind_detail
 
 urlpatterns = [
-    path('kind/all', kind_list, name='kind_list'),
+    path('kind/', kind_list, name='kind_list'),
     path('kind/create/', create_kind, name='create_kind'),
     path('kind/<int:pk>', kind_detail, name='kind_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,47 +1,41 @@
 from rest_framework.decorators import api_view
 from rest_framework import status
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 from api.models import Message, Money, Kind, Member
-from api.serializers import KindSerializer, MemberSerializer, MoneySerializer, MessageSerializer
+from .serializers import KindSerializer, MemberSerializer, MoneySerializer, MessageSerializer
 
 @api_view(['POST'])
 def create_kind(request):
-    data = JSONParser().parse(request)
-    serializer = KindSerializer(data=data)
+    serializer = KindSerializer(data=request.data)
     if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_202_ACCEPTED)
-    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def kind_list(request):
-    if request.method == 'GET':
-        kind = Kind.objects.all()
-        serializer = KindSerializer(kind, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+    kind = Kind.objects.all()
+    serializer = KindSerializer(kind, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def kind_detail(request, pk):
     try:
         kind = Kind.objects.get(pk=pk)
     except Kind.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = KindSerializer(kind)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = KindSerializer(kind, data=data)
+        serializer = KindSerializer(kind, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
         kind.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
