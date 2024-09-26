1f57289ff4f29e657dd25c6d653ace904f681998
jeshurun-com <jeshurunasefa@gmail.com>
2024-09-26 12:14:36 +0300

    databaseV2.0

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import validate_email
 from phonenumber_field.modelfields import PhoneNumberField
 
+# Message Table
 class Message(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField(validators=[validate_email])
@@ -10,11 +11,29 @@ class Message(models.Model):
     def __str__(self):
         return self.name
     
+#Kind Table
 class Kind(models.Model):
     type = models.CharField(max_length=100)
-    quantity = models.IntegerField(min_value=0)
-    name = models.CharField(max_length=150)
-    phone = PhoneNumberField(blank=False)
+    quantity = models.IntegerField(min_value=1)
+    name = models.CharField(max_length=50)
+    phone = PhoneNumberField(region=None,max_length=20,blank=False,null=False)
+    email = models.EmailField(validators=[validate_email])
+
+    def __str__(self):
+        return self.name
+
+#Money Table
+class Money(models.Model):
+    currency = models.CharField(max_length=5)
+    amount = models.IntegerField(min_value=1)
+    phone = PhoneNumberField(region=None,max_length=20,blank=False,null=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(validators=[validate_email])
+
+#Membership Table
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    phone = PhoneNumberField(region=None, max_length=20, blank=False, null=False)
     email = models.EmailField(validators=[validate_email])
 
     def __str__(self):
